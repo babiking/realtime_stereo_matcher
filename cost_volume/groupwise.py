dc6b146c4f4eabade5f51f72e6eba06ad224b668
babiking <brojackfeely@163.com>
2023-12-19 18:55:47 +0800

    unify cost volume dimension (N, C', H, W, D')

--- cost_volume/groupwise.py
+++ cost_volume/groupwise.py
@@ -32,21 +32,21 @@ class TorchGroupwiseCost(nn.Module):
             [4] max_disparity, Scalar/Int, maximum disparity value
 
         Return:
-            [1] volume, (N, n_groups, max_disparity, H, W), cost volume
+            [1] volume, (N, C', H, W, D'), i.e. (N, n_groups, H, W, max_disparity), cost volume
         """
         n, c, h, w = left.shape
 
-        volume = torch.zeros([n, self.n_groups, self.max_disparity, h, w])
+        volume = torch.zeros([n, self.n_groups, h, w, self.max_disparity])
 
         for i in range(self.max_disparity):
             if i == 0:
-                volume[:, :, i, :, :] = self.groupwise(
+                volume[:, :, :, :, i] = self.groupwise(
                     left,
                     right,
                     self.n_groups,
                 )
             else:
-                volume[:, :, i, :, i:] = self.groupwise(
+                volume[:, :, :, i:, i] = self.groupwise(
                     left[:, :, :, i:],
                     right[:, :, :, :-i],
                     self.n_groups,
