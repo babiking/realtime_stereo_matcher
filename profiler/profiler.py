4033c91153359eca1791e3fd1127bbac7d7d79a6
babiking <brojackfeely@163.com>
2023-12-19 15:57:01 +0800

    add flops count for profiler

--- profiler/profiler.py
+++ profiler/profiler.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 from thop import profile
+from fvcore.nn import FlopCountAnalysis, parameter_count_table
 
 
 def get_random_sample_data(n=1, c=64, h=480, w=640):
@@ -13,8 +14,13 @@ def get_model_capacity(module, inputs=None, verbose=True):
 
     n_macs, n_params = profile(module, inputs=inputs)
 
+    n_flops = FlopCountAnalysis(module, inputs=inputs).total()
+
+    # n_params = parameter_count_table(module)
+
     if verbose:
         print(f"module={module}.")
         print("{:<30}  {:<8}G".format("#operations:", np.round(n_macs / 1e9), 5))
+        print("{:<30}  {:<8}G".format("#flops: ", np.round(n_flops / 1e9, 5)))
         print("{:<30}  {:<8}M".format("#parameters: ", np.round(n_params / 1e6, 5)))
-    return n_macs, n_params
+    return n_macs, n_flops, n_params
