f29bd03adf1bced7e1415073750b95220b35dcb2
babiking <brojackfeely@163.com>
2023-12-19 18:57:36 +0800

    unify cost volume dimension (N, C', H, W, D')

--- cost_volume/concatenate.py
+++ cost_volume/concatenate.py
@@ -18,24 +18,24 @@ class TorchConcatenateCost(nn.Module):
             [3] max_disparity, Scalar/Int, max disparity value
 
         Return:
-            [1] volume, (N, 2 * C, max_disparity, H, W), cost volume
-                - volume[:, :C, d, y, x], left feature vector at pixel (x, y) aligned with disparity level d
-                - volume[:, :C, d, y, x], right feature vector at pixel (x, y) aligned with disparity level d
+            [1] volume, (N, 2 * C, H, W, max_disparity), cost volume
+                - volume[:, :C, y, x, d], left feature vector at pixel (x, y) aligned with disparity level d
+                - volume[:, :C, y, x, d], right feature vector at pixel (x, y) aligned with disparity level d
         """
         n, c, h, w = left.shape
 
         volume = torch.zeros(
-            size=(n, 2 * c, self.max_disparity, h, w),
+            size=(n, 2 * c, h, w, self.max_disparity),
             dtype=left.dtype,
             device=left.device,
         )
 
         for i in range(self.max_disparity):
             if i == 0:
-                volume[:, :c, i, :, :] = left
-                volume[:, c:, i, :, :] = right
+                volume[:, :c, :, :, i] = left
+                volume[:, c:, :, :, i] = right
             else:
-                volume[:, :c, i, :, i:] = left[:, :, :, i:]
-                volume[:, c:, i, :, i:] = right[:, :, :, :-i]
+                volume[:, :c, :, i:, i] = left[:, :, :, i:]
+                volume[:, c:, :, i:, i] = right[:, :, :, :-i]
         volume = volume.contiguous()
         return volume
