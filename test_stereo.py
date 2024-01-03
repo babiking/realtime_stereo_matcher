540a684d01afa02f84af93f61cf6af30b5674d2c
babiking <brojackfeely@163.com>
2024-01-03 15:22:10 +0800

    add build_model api

--- test_stereo.py
+++ test_stereo.py
@@ -6,7 +6,7 @@ import logging
 import numpy as np
 import torch
 import cv2 as cv
-from model.mobile_raft_stereo import MobileRaftStereoModel
+from model import build_model
 from dataset.input_padder import InputPadder
 from tools.pfm_file_io import write_pfm_file
 from tools.colorize import colorize_2d_matrix
@@ -17,18 +17,18 @@ gflags.DEFINE_string(
 )
 gflags.DEFINE_string(
     "model_chkpt_file",
-    "experiments/BASE/checkpoints/BASE-epoch-100000.pth.gz",
+    "experiments/BASE_STEREO_NET/checkpoints/BASE_STEREO_NET-epoch-100000.pth.gz",
     "model checkpont file",
 )
-gflags.DEFINE_string("name", "Motorcycle", "data name")
+gflags.DEFINE_string("name", "D435I", "data name")
 gflags.DEFINE_string(
     "left",
-    "/mnt/data/workspace/datasets/Middlebury/MiddEval3/trainingQ/Motorcycle/im0.png",
+    "/mnt/data/workspace/datasets/D435I/outdoor_blank_plane/image/100cm_left_Img.bmp",
     "left image file",
 )
 gflags.DEFINE_string(
     "right",
-    "/mnt/data/workspace/datasets/Middlebury/MiddEval3/trainingQ/Motorcycle/im1.png",
+    "/mnt/data/workspace/datasets/D435I/outdoor_blank_plane/image/100cm_right_Img.bmp",
     "right image file",
 )
 
@@ -51,16 +51,13 @@ def main():
 
     exp_config = json.load(open(FLAGS.exp_config_json, "r"))
 
-    model = torch.nn.DataParallel(MobileRaftStereoModel(**exp_config["model"])).to(
+    model = torch.nn.DataParallel(build_model(exp_config["model"])).to(
         torch.device("cuda" if torch.cuda.is_available() else "cpu")
     )
 
     model.cuda()
     model.eval()
 
-    assert FLAGS.model_chkpt_file.endswith(".pth") or FLAGS.model_chkpt_file.endswith(
-        ".pth.gz"
-    )
     logging.info(f"Loading checkpoint: {FLAGS.model_chkpt_file}...")
     checkpoint = torch.load(FLAGS.model_chkpt_file)
     model.load_state_dict(checkpoint, strict=True)
@@ -73,7 +70,7 @@ def main():
     # The CUDA implementations of the correlation volume prevent half-precision
     # rounding errors in the correlation lookup. This allows us to use mixed precision
     # in the entire forward pass, not just in the GRUs & feature extractors.
-    use_mixed_precision = exp_config["model"]["mixed_precision"]
+    use_mixed_precision = exp_config["model"].get("mixed_precision", True)
 
     image1 = cv.imread(FLAGS.left, cv.IMREAD_UNCHANGED)
     image2 = cv.imread(FLAGS.right, cv.IMREAD_UNCHANGED)
@@ -95,7 +92,7 @@ def main():
     flow_pr = padder.unpad(flow_pr).cpu().squeeze(0)
 
     flow_pr = -1.0 * np.squeeze(flow_pr.cpu().detach().numpy(), axis=0)
-    flow_color = colorize_2d_matrix(flow_pr, min_val=1.0, max_val=128.0)
+    flow_color = colorize_2d_matrix(flow_pr, min_val=1.0, max_val=200.0)
 
     data_path = os.getcwd()
     data_name = FLAGS.name
