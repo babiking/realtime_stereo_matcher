7455bda36484e9d64cd17e336e32b879f1c7100d
babiking <brojackfeely@163.com>
2024-01-04 13:39:27 +0800

    remove submodules

--- model/__init__.py
+++ model/__init__.py
@@ -1,11 +1,8 @@
 from model.mobile_stereo_net import MobileStereoNet
-from model.mobile_raft_net import MobileRaftNet
 
 
 def build_model(model_config):
     if model_config["type"] == "MobileStereoNet":
         return MobileStereoNet()
-    elif model_config["type"] == "MobileRaftNet":
-        return MobileRaftNet(**model_config["parameters"])
     else:
         raise NotImplementedError("unsupport model: {}".format(model_config["type"]))
