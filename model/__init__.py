81b446d792ccd0b93d8b13d574005ecd2bea9bc0
babiking <brojackfeely@163.com>
2024-01-05 10:56:30 +0800

    add mobile_stereo_net_v2

--- model/__init__.py
+++ model/__init__.py
@@ -1,8 +1,11 @@
 from model.mobile_stereo_net import MobileStereoNet
+from model.mobile_stereo_net_v2 import MobileStereoNetV2
 
 
 def build_model(model_config):
     if model_config["type"] == "MobileStereoNet":
         return MobileStereoNet()
+    if model_config["type"] == "MobileStereoNetV2":
+        return MobileStereoNetV2(**model_config["parameters"])
     else:
         raise NotImplementedError("unsupport model: {}".format(model_config["type"]))
