1fb31b1148e2b7e7c7790219d468f97dbb2621c7
babiking <brojackfeely@163.com>
2024-02-23 17:08:48 +0800

    add MobileResidualFeatureExtract

--- model/feature_extract.py
+++ model/feature_extract.py
@@ -8,20 +8,14 @@ def feature_extract_factory(config):
     extract_type = config["type"]
     if extract_type == "unet":
         return UNetFeatureExtract(**config["arguments"])
+    elif extract_type == "mobile_residual":
+        return MobileResidualFeatureExtract(**config["arguments"])
     else:
-        raise NotImplementedError(
-            f"invalid feature extractor type: {extract_type}!")
+        raise NotImplementedError(f"invalid feature extractor type: {extract_type}!")
 
 
 class BaseFeatureExtract(nn.Module):
-
-    def __init__(
-        self,
-        hidden_dims,
-        use_pretrain,
-        *args,
-        **kwargs,
-    ) -> None:
+    def __init__(self, hidden_dims, use_pretrain, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
         self.hidden_dims = hidden_dims
@@ -32,24 +26,23 @@ class BaseFeatureExtract(nn.Module):
 
     def forward(self, img):
         """
-            BaseFeatureExtractor to generate feature pyramid from low to high resolution.
+        BaseFeatureExtractor to generate feature pyramid from low to high resolution.
 
-            Args:
-                [1] image: N x 3 x H x W
-            
-            Return:
-                [1] feature_pyramid: e.g.
-                    [N x C4 x H/16 x W/16,
-                     N x C3 x H/8  x W/8,
-                     N x C2 x H/4  x W/4,
-                     N x C1 x H/2  x W/2,
-                     N x C0 x H/1  x W/1]
+        Args:
+            [1] image: N x 3 x H x W
+
+        Return:
+            [1] feature_pyramid: e.g.
+                [N x C4 x H/16 x W/16,
+                 N x C3 x H/8  x W/8,
+                 N x C2 x H/4  x W/4,
+                 N x C1 x H/2  x W/2,
+                 N x C0 x H/1  x W/1]
         """
         raise NotImplementedError
 
 
 class UNetFeatureExtract(BaseFeatureExtract):
-
     def __init__(self, hidden_dims, use_pretrain=False, *args, **kwargs):
         super().__init__(hidden_dims, use_pretrain, *args, **kwargs)
 
@@ -65,14 +58,14 @@ class UNetFeatureExtract(BaseFeatureExtract):
                     nn.LeakyReLU(0.2),
                 )
             elif i > 0 and i < self.down_factor:
-                layer = nn.Sequential(\
+                layer = nn.Sequential(
                     SameConv2d(self.hidden_dims[i - 1], self.hidden_dims[i], 4, 2),
                     nn.LeakyReLU(0.2),
                     nn.Conv2d(self.hidden_dims[i], self.hidden_dims[i], 3, 1, 1),
                     nn.LeakyReLU(0.2),
                 )
             elif i == self.down_factor:
-                layer = nn.Sequential(\
+                layer = nn.Sequential(
                     SameConv2d(self.hidden_dims[i - 1], self.hidden_dims[i], 4, 2),
                     nn.LeakyReLU(0.2),
                     nn.Conv2d(self.hidden_dims[i], self.hidden_dims[i], 3, 1, 1),
@@ -87,9 +80,11 @@ class UNetFeatureExtract(BaseFeatureExtract):
         for i in range(self.down_factor):
             j = self.down_factor - i
 
-            layer = UpMergeConvT2d(in_dim=self.hidden_dims[j],
-                                   out_dim=self.hidden_dims[j - 1],
-                                   cat_dim=self.hidden_dims[j - 1])
+            layer = UpMergeConvT2d(
+                in_dim=self.hidden_dims[j],
+                out_dim=self.hidden_dims[j - 1],
+                cat_dim=self.hidden_dims[j - 1],
+            )
             self.up_layers.append(layer)
 
     def get_down_factor(self):
@@ -108,3 +103,82 @@ class UNetFeatureExtract(BaseFeatureExtract):
             y = up_layer(up_pyramid[i], down_pyramid[j - 1])
             up_pyramid.append(y)
         return up_pyramid
+
+
+class MobileResidualFeatureExtract(BaseFeatureExtract):
+    def __init__(
+        self,
+        hidden_dims,
+        use_pretrain,
+        num_layers,
+        expanse_ratios=None,
+        dilations=None,
+        *args,
+        **kwargs,
+    ) -> None:
+        super().__init__(hidden_dims, use_pretrain, *args, **kwargs)
+        self.num_layers = num_layers
+        self.expanse_ratios = (
+            expanse_ratios
+            if expanse_ratios is not None
+            else [1 for _ in range(len(self.hidden_dims))]
+        )
+        self.dilations = (
+            dilations
+            if dilations is not None
+            else [1 for _ in range(len(self.hidden_dims))]
+        )
+        assert len(self.hidden_dims) == len(self.expanse_ratios) == len(self.dilations)
+
+        self.down_factor = self.get_down_factor()
+
+        self.down_layers = nn.ModuleList([])
+
+        for i in range(self.down_factor + 1):
+            layer = self.make_layer(
+                num_layers=self.num_layers[i - 1],
+                in_dim=(3 if i == 0 else self.hidden_dims[i - 1]),
+                out_dim=self.hidden_dims[i],
+                stride=(1 if i == 0 else 2),
+                expanse_ratio=self.expanse_ratios[i],
+                dilation=self.dilations[i],
+            )
+            self.down_layers.append(layer)
+
+    def get_down_factor(self):
+        return len(self.hidden_dims) - 1
+
+    def make_layer(self, num_layers, in_dim, out_dim, stride, expanse_ratio, dilation):
+        layers = []
+
+        layers.append(
+            MobileResidualBlockV2(
+                in_dim=in_dim,
+                out_dim=out_dim,
+                stride=stride,
+                expanse_ratio=expanse_ratio,
+                dilation=dilation,
+            )
+        )
+        layers.append(nn.LeakyReLU(0.2))
+
+        for _ in range(num_layers):
+            layers.append(
+                MobileResidualBlockV2(
+                    in_dim=out_dim,
+                    out_dim=out_dim,
+                    stride=1,
+                    expanse_ratio=1,
+                    dilation=1,
+                )
+            )
+            layers.append(nn.LeakyReLU(0.2))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        n = len(self.down_layers)
+
+        down_pyramid = [None for _ in range(n)]
+        for i, down_layer in enumerate(self.down_layers):
+            down_pyramid[n - 1 - i] = down_layer(x)
+        return down_pyramid
