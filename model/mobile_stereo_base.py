7ff876f3f354e58dd3d74c64371bd8048d2d5c71
babiking <brojackfeely@163.com>
2024-02-27 18:00:48 +0800

    output feature maps when train

--- model/mobile_stereo_base.py
+++ model/mobile_stereo_base.py
@@ -25,14 +25,21 @@ class MobileStereoBase(nn.Module):
         self.regress = disparity_regress_factory(model_config["disparity_regress"])
         self.refine = disparity_refine_factory(model_config["disparity_refine"])
 
-    def align(self, img, down_factor):
+    def align(self, img):
         n, c, src_h, src_w = img.shape
 
-        divisor = 2**down_factor
+        divisor = 2**self.down_factor
 
         h_pad = (divisor - (src_h % divisor)) % divisor
         w_pad = (divisor - (src_w % divisor)) % divisor
         return h_pad, w_pad
+
+    def pad(self, img):
+        h_pad, w_pad = self.align(img)
+
+        if h_pad != 0 or w_pad != 0:
+            img = F.pad(img, (0, w_pad, 0, h_pad))
+        return img
 
     def upsample(self, disp, dst_size):
         src_h, src_w = disp.shape[2:]
@@ -46,15 +53,15 @@ class MobileStereoBase(nn.Module):
         return disp
 
     def forward(self, l_img, r_img, is_train=True):
-        _, _, h, w = l_img.shape
+        _, _, src_h, src_w = l_img.shape
 
         l_img = (2.0 * (l_img / 255.0) - 1.0).contiguous()
         r_img = (2.0 * (r_img / 255.0) - 1.0).contiguous()
 
-        h_pad, w_pad = self.align(l_img, self.down_factor)
-        if h_pad != 0 or w_pad != 0:
-            l_img = F.pad(l_img, (0, w_pad, 0, h_pad))
-            r_img = F.pad(r_img, (0, w_pad, 0, h_pad))
+        l_img = self.pad(l_img)
+        r_img = self.pad(r_img)
+
+        _, _, dst_h, dst_w = l_img.shape
 
         l_fmaps = self.extract(l_img)
         r_fmaps = self.extract(r_img)
@@ -66,8 +73,10 @@ class MobileStereoBase(nn.Module):
         l_disp_pyramid = self.refine(
             l_disp, l_fmaps[1:], r_fmaps[1:], is_train=is_train
         )
-        l_disp_pyramid = [
-            self.upsample(l_disp, l_img.shape[2:])[:, :, :h, :w]
-            for l_disp in l_disp_pyramid
-        ]
-        return l_disp_pyramid
+
+        if is_train:
+            return ([l_disp] + l_disp_pyramid, l_fmaps, r_fmaps)
+        else:
+            if src_h != dst_h or src_w != dst_w:
+                l_disp_pyramid[-1] = l_disp_pyramid[-1][:, :, :src_h, :src_w]
+            return l_disp_pyramid
