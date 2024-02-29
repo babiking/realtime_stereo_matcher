1fc421a38b3fad4dec31b503020e920c1b5bef37
babiking <brojackfeely@163.com>
2024-02-29 16:01:33 +0800

    add ContextUpsample module

--- model/disparity_refine.py
+++ model/disparity_refine.py
@@ -8,6 +8,8 @@ def disparity_refine_factory(config):
     refine_type = config["type"]
     if refine_type == "dilate":
         return DilateRefineNet(**config["arguments"])
+    elif refine_type == "context":
+        return ContextRefineNet(**config["arguments"])
     else:
         raise NotImplementedError(f"invalid refine net type: {refine_type}!")
 
@@ -16,8 +18,104 @@ class BaseRefineNet(nn.Module):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-    def forward(self, l_disp, l_fmaps=None, r_fmaps=None):
+    def forward(self, l_disp, l_fmaps=None, r_fmaps=None, is_train=True):
         raise NotImplementedError
+
+
+class ContextRefineNet(BaseRefineNet):
+    def __init__(
+        self,
+        in_dim,
+        hidden_dims,
+        context_dilates,
+        fold_radius,
+        *args,
+        **kwargs,
+    ) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.in_dim = in_dim
+        self.hidden_dims = hidden_dims
+        self.context_dilates = context_dilates
+        self.fold_radius = fold_radius
+
+        self.context = nn.Sequential(
+            get_conv2d_1x1(
+                in_dim=self.in_dim,
+                out_dim=self.hidden_dims[0],
+                stride=1,
+                dilation=self.context_dilates[0],
+                norm_type="batch",
+                use_relu=True,
+            ),
+            *[
+                ResidualBlock(
+                    in_dim=self.hidden_dims[i - 1],
+                    out_dim=self.hidden_dims[i],
+                    norm_type="batch",
+                    stride=1,
+                    dilation=self.context_dilates[i],
+                )
+                for i in range(1, len(self.hidden_dims))
+            ],
+            nn.Conv2d(self.hidden_dims[-1], (self.fold_radius ** 2), 3, 1, 1),
+        )
+
+    def upsample(self, l_disp, l_mask):
+        """
+            Upsample disparity map by convex combination.
+
+            Args:
+                [1] l_disp: N x 1 x H x W
+                [2] l_mask: N x 9 x (8 * H) x (8 * W)
+
+            Return:
+                [1] l_disp: N x 1 x H x W
+
+        l_mask.view([N, 9, 8, H/8, 8, W/8])
+        l_mask.softmax(dim=1)
+
+        l_disp.unfold([N, 9, 1, H/8, 1, W/8])
+
+        l_disp = (l_disp * l_mask).sum(dim=1)
+
+        l_disp.reshape([N, 1, H, W])
+        """
+        n, _, h0, w0 = l_disp.shape
+        n, c1, h1, w1 = l_mask.shape
+
+        assert c1 == (self.fold_radius**2)
+
+        assert w1 % w0 == 0
+        up_factor = int(w1 / w0)
+
+        l_mask = torch.softmax(l_mask, dim=1)
+        l_mask = l_mask.view([n, c1, up_factor, h0, up_factor, w0])
+
+        l_disp_up = F.unfold(
+            l_disp, kernel_size=[self.fold_radius, self.fold_radius], padding=1
+        ) * float(up_factor)
+        l_disp_up = l_disp_up.view([n, c1, 1, h0, 1, w0])
+        l_disp_up = (l_disp_up * l_mask).mean(1).squeeze(1)
+
+        l_disp_up = l_disp_up.view([n, 1, h1, w1])
+        return l_disp_up
+
+    def forward(self, l_disp, l_fmaps=None, r_fmaps=None, is_train=True):
+        assert l_fmaps[-1].shape[-1] % l_disp.shape[-1] == 0
+        assert l_fmaps[-1].shape[-2] % l_disp.shape[-2] == 0
+
+        l_mask = self.context(l_fmaps[-1])
+
+        l_disp_up = self.upsample(l_disp, l_mask)
+        if is_train:
+            return (
+                [l_disp, l_disp_up],
+                [l_fmaps[0], l_fmaps[-1]],
+                [r_fmaps[0], r_fmaps[-1]],
+            )
+        else:
+            return [l_disp_up]
 
 
 class DilateRefineBlock(nn.Module):
@@ -143,4 +241,8 @@ class DilateRefineNet(BaseRefineNet):
                 continue
 
             l_disp_pyramid.append(l_disp)
-        return l_disp_pyramid
+
+        if is_train:
+            return [l_disp] + l_disp_pyramid, l_fmaps, r_fmaps
+        else:
+            return l_disp_pyramid
