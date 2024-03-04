9e665cad99afb736b019790af733ada8f2869b81
babiking <brojackfeely@163.com>
2024-03-04 16:37:36 +0800

    add other fast_acv_net

--- model/__init__.py
+++ model/__init__.py
@@ -3,6 +3,7 @@ from model.mobile_stereo_net_v2 import MobileStereoNetV2
 from model.mobile_stereo_net_v3 import MobileStereoNetV3
 from model.mobile_stereo_net_v4 import MobileStereoNetV4
 from model.opencv_sgbm_module import OpenCVSGBMModule
+from others.fast_acv_net.fast_acv_net import Fast_ACVNet
 
 
 def build_model(model_config):
@@ -16,5 +17,7 @@ def build_model(model_config):
         return MobileStereoNetV4(**model_config["parameters"])
     elif model_config["type"] == "OpenCVSGBMModule":
         return OpenCVSGBMModule(**model_config["parameters"])
+    elif model_config["type"] == "OtherFastACVNet":
+        return Fast_ACVNet(**model_config["parameters"])
     else:
         raise NotImplementedError("unsupport model: {}".format(model_config["type"]))
